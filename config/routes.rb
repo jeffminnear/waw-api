d9ee9a67e41030f0b90a90d3ec10015ab058c323
Jeff Minnear <jeffminnear@gmail.com>
2015-12-11 14:27:43 -0500

    Add PUT#update and DELETE#destroy routes to api/goals

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:index, :create, :destroy] do
-      resources :goals, only: :create
+    resources :users, only: [:index, :create] do
+      resources :goals, only: [:create, :update, :destroy]
     end
   end
 
