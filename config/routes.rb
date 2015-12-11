e466883f4fd7dd58013ec350bea86262c0ca63e0
Jeff Minnear <jeffminnear@gmail.com>
2015-12-11 13:00:31 -0500

    Add POST#create route /api/users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: :index do
+    resources :users, only: [:index, :create] do
       resources :goals, only: :create
     end
   end
