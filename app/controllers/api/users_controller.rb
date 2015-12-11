9052eaf32971bf93f9c7d8b451314ae4b7ee55fc
Jeff Minnear <jeffminnear@gmail.com>
2015-12-11 12:53:44 -0500

    Add #create action to Api::UsersController

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,4 +6,21 @@ class Api::UsersController < ApiController
 
     render json: users, each_serializer: UserSerializer
   end
+
+  def create
+    user = User.new(users_params)
+
+    if user.save
+      render json: user
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def users_params
+    params.require(:user).permit(:email, :password)
+  end
 end
