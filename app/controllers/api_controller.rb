24cabe33bcabc42bdf604be24aeb3eaacd1c1bc0
Jeff Minnear <jeffminnear@gmail.com>
2015-12-11 09:05:37 -0500

    Fix api_controller.rb

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,10 +1,13 @@
 class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
-
+  skip_before_action :authenticate_user!
 
   private
 
   def authenticated?
-    authenticate_or_request_with_http_basic {|email, encrypted_password| User.where( email: email, encrypted_password: encrypted_password).present? }
+    authenticate_or_request_with_http_basic do |email, password|
+      user = User.find_for_database_authentication(email: email)
+      user && user.valid_for_authentication? { user.valid_password?(password) }
+    end
   end
 end
