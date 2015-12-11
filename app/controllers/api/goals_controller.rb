338ef730abba8ab995d26da9d629fbe7a4b267f8
Jeff Minnear <jeffminnear@gmail.com>
2015-12-11 13:29:52 -0500

    Update Api::GoalsController#create

--- app/controllers/api/goals_controller.rb
+++ app/controllers/api/goals_controller.rb
@@ -2,5 +2,20 @@ class Api::GoalsController < ApiController
   before_action :authenticated?
 
   def create
+    user = User.find(params[:user_id])
+    goal = user.goals.build(goal_params)
+
+    if goal.save
+      render json: goal
+    else
+      render json: { errors: goal.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:name)
   end
 end
