ad8ad05e695404b1ba62c3828cf56e97749e9d84
Jeff Minnear <jeffminnear@gmail.com>
2015-12-14 10:29:47 -0500

    Override #authenticate_user! to enable #current_user in API controllers

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,13 +1,14 @@
 class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
-  skip_before_action :authenticate_user!
 
   private
 
-  def authenticated?
+  def authenticate_user!
     authenticate_or_request_with_http_basic do |email, password|
       user = User.find_for_database_authentication(email: email)
-      user && user.valid_for_authentication? { user.valid_password?(password) }
+      authenticated = user && user.valid_for_authentication? { user.valid_password?(password) }
+      @current_user = user if authenticated
+      authenticated
     end
   end
 end
