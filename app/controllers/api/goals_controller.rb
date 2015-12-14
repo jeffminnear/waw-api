c1feb6a125941345605abe0fbc5dd2f914c4ab81
Jeff Minnear <jeffminnear@gmail.com>
2015-12-14 10:32:59 -0500

    Rewrite Api::GoalsController methods to be scoped to current_user

--- app/controllers/api/goals_controller.rb
+++ app/controllers/api/goals_controller.rb
@@ -1,14 +1,11 @@
 class Api::GoalsController < ApiController
-  before_action :authenticated?
 
   def index
-    user = User.last # VERY TEMPORARY
-    render json: user.goals, each_serializer: GoalSerializer
+    render json: current_user.goals, each_serializer: GoalSerializer
   end
 
   def create
-    user = User.find(params[:user_id])
-    goal = user.goals.build(goal_params)
+    goal = current_user.goals.build(goal_params)
 
     if goal.save
       render json: goal
@@ -18,22 +15,26 @@ class Api::GoalsController < ApiController
   end
 
   def update
-    goal = Goal.find(params[:id])
+    begin
+      goal = current_user.goals.find(params[:id])
 
-    if goal.update(goal_params)
-      render json: goal
-    else
-      render json: { errors: goal.errors.full_messages }, status: :unprocessable_entity
+      if goal.update(goal_params)
+        render json: goal
+      else
+        render json: { errors: goal.errors.full_messages }, status: :unprocessable_entity
+      end
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "The specified goal was not found" }, status: :not_found
     end
   end
 
   def destroy
     begin
-      goal = Goal.find(params[:id])
+      goal = current_user.goals.find(params[:id])
       goal.destroy
-      render json: {}, status: :no_content
+      render json: { message: "The specified goal was deleted" }, status: :no_content
     rescue ActiveRecord::RecordNotFound
-      render json: {}, status: :not_found
+      render json: { error: "The specified goal was not found" }, status: :not_found
     end
   end
 
