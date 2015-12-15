14a316a50a915ad124419b0e7640f09e7206c07b
Jeff Minnear <jeffminnear@gmail.com>
2015-12-15 13:38:54 -0500

    Add tests for PUT#update to goals_api_spec.rb

--- spec/api/goals_api_spec.rb
+++ spec/api/goals_api_spec.rb
@@ -58,4 +58,34 @@ describe '/api/goals' do
       end
     end
   end
+
+  context 'PUT#update' do
+    it 'updates the goal with the correct parameters' do
+      user = create(:user)
+      goal = create(:goal, user: user, name: "the original name")
+      headers = json_request_headers
+      headers['Authorization'] = ActionController::HttpAuthentication::Basic.encode_credentials(user.email,user.password)
+      params = { name: "a new name that is different" }
+
+      put '/api/goals/1', params.to_json, headers
+      json = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:ok)
+      expect(json["goal"]).to eq({ "id" => 1, "name" => "a new name that is different", "created_at" => Time.now.strftime('%B %d %Y') })
+    end
+
+    it 'returns an error if the requested goal can not be found' do
+      user = create(:user)
+      goal = create(:goal, user: user, name: "the original name")
+      headers = json_request_headers
+      headers['Authorization'] = ActionController::HttpAuthentication::Basic.encode_credentials(user.email,user.password)
+      params = { name: "a new name that is different" }
+
+      put '/api/goals/2', params.to_json, headers
+      json = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:not_found)
+      expect(json).to eq({ "error" => "The specified goal was not found" })
+    end
+  end
 end
